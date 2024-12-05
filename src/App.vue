d743644a87b3bba3208e5085194b6a07c518953a
Avishka Madushanka <avishka.madushanka123@gmail.com>
2024-12-06 00:01:40 +0530

    new changes

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0);
+
+</script>
+
+<template>
+  <div>
+    <h4> The current count is...</h4>
+    <h1>{{count}}</h1>
+    <button @click="count--" >-</button>
+    <button @click="count++" >+</button>
+  </div>
+</template>
